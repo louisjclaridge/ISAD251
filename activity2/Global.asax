658563f20b028ca8a53a6887e15b06b072d5cfc0
Louis Claridge <ljclaridge@socnet.soc.plymouth.ac.uk>
2018-10-16 10:18:06 +0100

    Added task 2 of Unit 2 - Practical 1

---
+++ activity2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="activity2.MvcApplication" Language="C#" %>
---
+++ activity2/Models/OrderLines.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace activity2.Models
+{
+    public class OrderLines
+    {
+    }
+}
